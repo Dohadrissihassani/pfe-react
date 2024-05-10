8586f8257fe429f3e17c1bcb839f931ce23352c9
Dohadrissihassani <dohadrissihassani@gmail.com>
2024-05-10 21:11:31 +0100

    add professeur

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 import ModifierVotreCompte from './pages/ModifierVotreCompte';
 import NavBar from './pages/NavBar';
 import SideBar from './pages/SideBar';
+import Professeur from './component/professeur/Professeur';
+import AddProfesser from './component/professeur/addProfesser/AddProfesser';
+import EditProfessur from './component/professeur/EditProfesseur/EditProfessur';
 
 function App() {
   return (
    <>
-     <NavBar/>
+     
+    <Router>
+    <NavBar/>
      <SideBar/>
+     <Switch>
+        <Route exact path="/" > 
+         <Professeur/>
+       </Route>
+        <Route exact path="/add" > 
+         <AddProfesser/>
+       </Route>
+       <Route exact path="/edit/:id" render={ props=> <EditProfessur/>} > 
+         <EditProfessur/>
+       </Route>
+
+     </Switch>
+  
+    </Router>
+
      </>
 
     
